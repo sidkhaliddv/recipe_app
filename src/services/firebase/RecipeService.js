54882e8b5a24ce524fd6f6072043f95cd3b0fd92
sidkhaliddv <sidkhaliddv@gmail.com>
2024-08-26 01:26:27 +0500

    added update recipe functionality

--- src/services/firebase/RecipeService.js
+++ src/services/firebase/RecipeService.js
@@ -1,4 +1,4 @@
-import { collection, addDoc, getDocs } from "firebase/firestore"; 
+import { collection, addDoc, getDocs, getDoc, doc, updateDoc } from "firebase/firestore"; 
 import { db } from "../../../firebase";
 
 const COLLECTION_NAME = 'recipes'
@@ -13,11 +13,29 @@ class RecipeService {
     try {
       const collectionRef = collection(db, COLLECTION_NAME)
       const savedRecipe = await addDoc(collectionRef, recipe)
-      console.log('recipe saved')
       return savedRecipe
     } catch(e) {
       console.log(e)
     }
+  }
+
+  async get_recipe(id) {
+    console.log(id)
+    if(!id) return null;
+
+    const recipe = await getDoc(doc(db, COLLECTION_NAME, id))
+    if (recipe.exists()){
+      return recipe.data()
+    } else {
+      return null;
+    }
+  }
+
+  async update_recipe(id, recipe) {
+    if(!id) return null;
+
+    const updatedRecipe = updateDoc(doc(db, 'recipes', id), recipe)
+    return updatedRecipe
   }
 }
 
