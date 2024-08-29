a2bdbcf813ca2b7c45401ef48c4a09305a14c9aa
sidkhaliddv <sidkhaliddv@gmail.com>
2024-08-29 15:38:46 +0500

    clicking Recipes at left user should be directed to '/recipes' * Add a new button on right 'My Recipes' * 'My Recipes' should only be visible when logged in

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,11 +16,16 @@ const Navbar = () => {
       <nav className="bg-gray-200 p-4">
         <div className="flex justify-between">
           <div className="flex">
-            <h1 className="inline-grid items-center">My Recipes</h1>
-            <input type="text" name="" placeholder="Search Recipes..." className="ml-2 p-2 bg-gray-200 border border-cyan-400 rounded-lg focus:outline-none" id="" />
+            <button className="inline-grid items-center" onClick={()=>navigate('/recipes')}>My Recipes</button>
+            <input type="text" name="" placeholder="Search Recipes..." className="ml-2 p-2 bg-gray-200 border border-cyan-400 rounded-lg focus:outline-none hover:shadow-xl hover:border-cyan-600 focus:shadow-xl focus:border-cyan-600" id="" />
           </div>
-          <div className="inline-grid items-center">
-            <button type="button" className="border-2 border text-1xl p-3 bg-gray-200 hover:bg-gray-400 hover:text-white" onClick={handleClick}>{currentUser ? 'Logout' : 'Sign in'}</button>
+          <div className="flex mx-2">
+            {currentUser && <div className="inline-grid">
+              <button type="button" className="border-2 border text-1xl p-3 bg-gray-200 hover:bg-gray-400 hover:text-white" onClick={()=>navigate('/recipes/my')}>My Recipes</button>
+            </div>}
+            <div className="">
+              <button type="button" className="border-2 border text-1xl p-3 bg-gray-200 hover:bg-gray-400 hover:text-white" onClick={handleClick}>{currentUser ? 'Logout' : 'Sign in'}</button>
+            </div>
           </div>
         </div>
       </nav>
